73d4ad351616b66e72f20a9de5a9bfb2906351b6
abedinmolla2025 <abedinmolla2025@gmail.com>
2025-12-29 16:26:56 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
-import { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "app.lovable.22946abbf65a4dc099a7309f0d5611c4",
-  appName: "A Lovable project",
-  webDir: "dist",
-  server: {
-    url: "https://22946abb-f65a-4dc0-99a7-309f0d5611c4.lovableproject.com?forceHideBadge=true",
-    cleartext: true,
-  },
-};
-
-export default config;
+{
+  "appId": "com.noor.app",
+  "appName": "NOOR",
+  "webDir": "www",
+  "server": {
+    "url": "https://remix-of-remix-of-remix-of-islamic.vercel.app",
+    "cleartext": true
+  }
+}
